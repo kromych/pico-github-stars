7eb7f14d6ff7833139cbf6bc9a7b6acc9b311388
kromych <kromych@users.noreply.github.com>
2024-10-20 22:20:31 -0700

    Parse json, refactor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+//! This is the main file for the project.
+//!
+//! * It initializes the peripherals, the network stack, and then
+//! * enters a loop where it calls data processing functions.
+
 #![no_std]
 #![no_main]
 #![allow(async_fn_in_trait)]
@@ -6,25 +11,18 @@ use cyw43_pio::PioSpi;
 use defmt::*;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_net::dns::DnsSocket;
-use embassy_net::tcp::TcpSocket;
-use embassy_net::{Config, IpAddress, IpEndpoint, Ipv4Address};
+use embassy_net::Config;
 use embassy_rp::bind_interrupts;
 use embassy_rp::clocks::RoscRng;
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::{DMA_CH0, PIO0};
 use embassy_rp::pio::{InterruptHandler, Pio};
 use embassy_time::{Duration, Timer};
-use embedded_io_async::Write;
-use embedded_nal_async::{AddrType, Dns, IpAddr};
-use embedded_tls::{
-    Aes128GcmSha256, TlsConfig, TlsConnection, TlsContext, TlsError, UnsecureProvider,
-};
 use panic_probe as _;
-use rand::{RngCore, SeedableRng};
-use rand_chacha::ChaCha8Rng;
-use serde::Deserialize;
+use rand::RngCore;
 use static_cell::StaticCell;
+
+mod process_data;
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
@@ -33,14 +31,6 @@ bind_interrupts!(struct Irqs {
 const WIFI_NETWORK: &str = env!("WIFI_NETWORK");
 const WIFI_PASSWORD: &str = env!("WIFI_PASSWORD");
 const DEFAULT_SLEEP_TIME_SEC: u64 = 60;
-const HOST: &str = env!("HOST");
-const REQ_HEADERS: &[u8] = b"GET /repos/kromych/pico-github-stars HTTP/1.1\r\n\
-Host: api.github.com\r\n\
-User-Agent: kromych/pico-github-stars\r\n\
-Accept: application/vnd.github+json\r\n\
-X-GitHub-Api-Version:2022-11-28\r\n\
-Connection: close\r\n\
-\r\n";
 
 #[embassy_executor::task]
 async fn cyw43_task(
@@ -52,27 +42,6 @@ async fn cyw43_task(
 #[embassy_executor::task]
 async fn net_task(runner: &'static embassy_net::Stack<cyw43::NetDriver<'static>>) -> ! {
     runner.run().await
-}
-
-#[allow(dead_code)]
-fn process_data(body: &str) {
-    debug!("Response body: {:?}", &body);
-
-    #[derive(Deserialize)]
-    struct ApiResponse {
-        stargazers_count: u32,
-        // other fields as needed
-    }
-
-    let bytes = body.as_bytes();
-    match serde_json_core::de::from_slice::<ApiResponse>(bytes) {
-        Ok((output, _used)) => {
-            debug!("STARS: {:?}", output.stargazers_count);
-        }
-        Err(_e) => {
-            error!("Failed to parse response body");
-        }
-    }
 }
 
 // Static cells for holding state, resources, and stack
@@ -165,135 +134,6 @@ async fn wait_for_network(stack: &'static embassy_net::Stack<cyw43::NetDriver<'s
     info!("Stack is up!");
 }
 
-async fn read_data_from_rest_api(
-    stack: &'static embassy_net::Stack<cyw43::NetDriver<'static>>,
-    seed: u64,
-) {
-    let dns_client = DnsSocket::new(stack);
-
-    let mut rx_buffer = [0; 8192];
-    let mut tx_buffer = [0; 8192];
-
-    debug!("resolving {}", HOST);
-    let remote_addr = dns_client.get_host_by_name(HOST, AddrType::IPv4).await;
-    let remote_addr = match remote_addr {
-        Ok(addr) => addr,
-        Err(e) => {
-            error!("DNS error: {:?}", e);
-            return;
-        }
-    };
-
-    let remote_endpoint = match remote_addr {
-        IpAddr::V4(addr) => IpEndpoint::new(IpAddress::Ipv4(Ipv4Address(addr.octets())), 443),
-        _ => defmt::unreachable!("IPv6 not supported"),
-    };
-    debug!("connecting to {}", remote_endpoint);
-
-    let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
-    socket.set_timeout(Some(Duration::from_secs(10)));
-
-    let r = socket.connect(remote_endpoint).await;
-    if let Err(e) = r {
-        warn!("connect error: {:?}", e);
-        return;
-    }
-    log::debug!("TCP connected!");
-
-    let mut read_record_buffer = [0; 16640];
-    let mut write_record_buffer = [0; 16640];
-    let config = TlsConfig::new();
-    let mut tls = TlsConnection::new(socket, &mut read_record_buffer, &mut write_record_buffer);
-
-    if let Err(e) = tls
-        .open(TlsContext::new(
-        &config,
-        UnsecureProvider::new::<Aes128GcmSha256>(ChaCha8Rng::seed_from_u64(seed)),
-    ))
-    .await
-    {
-        error!("TLS error: {:?}", e);
-        return;
-    }
-
-    if let Err(e) = tls.write_all(REQ_HEADERS).await {
-        error!("write error: {:?}", e);
-        return;
-    }
-    if let Err(e) = tls.flush().await {
-        error!("flush error: {:?}", e);
-        return;
-    }
-
-    let mut rx_buf = [0; 16384];
-    let sz = {
-        let mut pos = 0;
-        loop {
-            let sz = match tls.read(&mut rx_buf[pos..]).await {
-                Ok(sz) => sz,
-                Err(TlsError::ConnectionClosed) => {
-                    debug!("Connection closed by the server");
-                    break;
-                }
-                Err(e) => {
-                    error!("read error: {:?}", e);
-                    return;
-                }
-            };
-            if sz == 0 {
-                break;
-            }
-            pos += sz;
-        }
-        info!("Read {} bytes from the server", pos);
-        pos
-    };
-
-    let mut pos = 0;
-    while pos < sz {
-        let tail = &rx_buf[pos..];
-        let line_end = match tail.iter().position(|&c| c == b'\n') {
-            Some(i) => i,
-            None => {
-                warn!("no newline found");
-                break;
-            }
-        };
-
-        let line = &rx_buf[pos..pos + line_end];
-        let line = match core::str::from_utf8(line) {
-            Ok(line) => line,
-            Err(_) => {
-                warn!("invalid utf8");
-                continue;
-            }
-        };
-        if !line.ends_with('\r') {
-            warn!("no CR found");
-            return;
-        }
-        let line = &line[..line.len() - 1];
-
-        if pos == 0 {
-            if !line.starts_with("HTTP/1.1 2") {
-                error!("HTTP error: {:?}", line);
-            } else {
-                info!("HTTP status: {:?}", line);
-            }
-        } else if !line.is_empty() {
-            debug!("HTTP header: {:?}", line);
-        }
-
-        // Skip the CRLF.
-        pos += line.len() + 2;
-
-        if line.is_empty() {
-            info!("Size of headers: {}", pos);
-            break;
-        }
-    }
-}
-
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     info!(
@@ -307,7 +147,7 @@ async fn main(spawner: Spawner) {
     wait_for_network(stack).await;
 
     loop {
-        read_data_from_rest_api(stack, rng.next_u64()).await;
+        process_data::read_data(stack).await;
 
         let sleep_sec = env!("SLEEP_TIME_SEC")
             .parse()
